5587bff6fc878c02719d8e88b5727ca9ac500155
siddharth sharma <sandesh251189@gmail.com>
2018-03-07 03:01:31 -0500

    data coming from google api to console

--- routes/aouth-routes.js
+++ routes/aouth-routes.js
@@ -14,7 +14,7 @@ router.get('/google',passport.authenticate('google',{
   scope:['profile']
 }) )
 
-router.get('/google/redirect', (req, res) => {
+router.get('/google/redirect',passport.authenticate('google'), (req, res) => {
   res.send("You have reached at calback URI")
 })
 module.exports = router;
